840f90faaba5b393da92cfaf0c65909b421422c3
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-09-28 06:27:38 +0100

    Lock boxes implementation

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    n = len(boxes)
+    if n == 0:
+        return True  # No boxes, so nothing to unlock.
+
+    # Initialize a set to keep track of the opened boxes.
+    opened = set()
+    opened.add(0)  # The first box is already unlocked.
+
+    # Initialize a stack to perform DFS.
+    stack = [0]
+
+    while stack:
+        current_box = stack.pop()
+        keys = boxes[current_box]
+
+        # Iterate through the keys in the current box.
+        for key in keys:
+            # Check if the key opens a new box and it's not already opened.
+            if key >= 0 and key < n and key not in opened:
+                opened.add(key)
+                stack.append(key)
+
+    # If all boxes have been opened, return True.
+    return len(opened) == n
