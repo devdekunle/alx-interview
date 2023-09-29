ce8c0eb8375cb8c1a80ceeafb6c324ab664980f9
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-09-29 07:50:32 +0100

    refactor code

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,47 +4,58 @@ Prime game implementation
 """
 
 
+def is_prime(num):
+    """
+    prime game implementation
+    """
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def get_primes(n):
+    primes = []
+    for num in range(2, n + 1):
+        if is_prime(num):
+            primes.append(num)
+    return primes
+
+
+def can_win(nums):
+    primes = get_primes(max(nums))
+    memo = {}
+
+    def helper(nums):
+        if tuple(nums) in memo:
+            return memo[tuple(nums)]
+        for num in nums:
+            if num in primes:
+                new_nums = [x for x in nums if x % num != 0]
+                if not helper(new_nums):
+                    memo[tuple(nums)] = True
+                    return True
+        memo[tuple(nums)] = False
+        return False
+
+    return helper(nums)
+
+
 def isWinner(x, nums):
-    """
-    Prime game dynamic programming
-    """
-    def is_prime(num):
-        if num <= 1:
-            return False
-        if num == 2:
-            return True
-        if num % 2 == 0:
-            return False
-        for i in range(3, int(num**0.5) + 1, 2):
-            if num % i == 0:
-                return False
-        return True
-
-    def play_round(n):
-        # A dynamic programming table to
-        # memoize the winners for each number from 1 to n.
-        # dp[i] will be True if Maria can win when starting with i.
-        dp = [False] * (n + 1)
-
-        for i in range(2, n + 1):
-            if is_prime(i):
-                dp[i] = not any(dp[i - j]
-                                for j in range(2, i + 1) if i % j == 0)
-
-        return dp[n]
-
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
-        if play_round(n):
+        if can_win(list(range(1, n + 1))):
             maria_wins += 1
         else:
             ben_wins += 1
 
     if maria_wins > ben_wins:
         return "Maria"
-    elif ben_wins > maria_wins:
+    elif maria_wins < ben_wins:
         return "Ben"
     else:
         return None
